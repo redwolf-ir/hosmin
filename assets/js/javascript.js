beb5698d2ecab563316bc4c285d5136f9a0bf956
Red Wolf ir <redwolf_ir@outlook.com>
2021-04-02 13:31:23 +0430

    add some new sections

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -42,21 +42,77 @@ $("#wrapper").fullpage({
 
   onLeave: function (index, nextIndex, direction) {
     if (index == 1 && nextIndex == 2) {
+      gsap.fromTo(
+        ".curly-line-3",
+        { opacity: 0, x: -50 },
+        { duration: 2, opacity: 1, x: 0, ease: Power2.easeInOut },
+        0
+      );
+      gsap.fromTo(
+        ".curly-line-4",
+        { opacity: 0, x: 50 },
+        { duration: 2, opacity: 1, x: 0, ease: Power2.easeInOut },
+        0
+      );
+      gsap.fromTo(
+        "#countdown",
+        { opacity: 0, y: -100 },
+        { duration: 2, opacity: 1, y: 0, ease: Power2.easeInOut },
+        0
+      );
+      gsap.fromTo(
+        ".featured picture",
+        { opacity: 0, y: 100 },
+        { duration: 2, opacity: 1, y: 0, ease: Power2.easeInOut },
+        0
+      );
+      gsap.fromTo(
+        ".featured .content h3",
+        { opacity: 0, scale: 1.5 },
+        { duration: 2, opacity: 1, scale: 1, ease: Power2.easeInOut },
+        0
+      );
+    } else if (index == 2 && nextIndex == 3) {
       var t2 = gsap.timeline();
 
       t2.fromTo(
         ".works .content",
         { opacity: 0, x: -50 },
-        { duration: 3.5, opacity: 1, x: 0, ease: Power2.easeInOut },
+        { duration: 2, opacity: 1, x: 0, ease: Power2.easeInOut },
         0
       ).fromTo(
         ".last_works",
         { opacity: 0, y: 100 },
-        { duration: 2.5, delay: 1, opacity: 1, y: 0, ease: Power2.easeInOut },
+        { duration: 2, delay: 1, opacity: 1, y: 0, ease: Power2.easeInOut },
         0
       );
     } else if (index == 3 && direction == "down") {
-      alert("hello");
+      var t3 = gsap.timeline();
+
+      t3.fromTo(
+        ".new-release .content h2",
+        { opacity: 0, x: -50 },
+        { duration: 1.5, opacity: 1, x: 0, ease: Power2.easeInOut },
+        0
+      )
+        .fromTo(
+          ".new-release .content span",
+          { opacity: 0, x: 50 },
+          { duration: 1.5, opacity: 1, x: 0, ease: Power2.easeInOut },
+          0
+        )
+        .fromTo(
+          ".new-release .content ul",
+          { opacity: 0, y: -150 },
+          { duration: 2.5, opacity: 1, y: 0, ease: Power2.easeInOut },
+          0
+        )
+        .fromTo(
+          ".album-info",
+          { opacity: 0, y: 150 },
+          { duration: 2.5, opacity: 1, y: 0, ease: Power2.easeInOut },
+          0
+        );
     }
   },
 });
@@ -125,9 +181,39 @@ $("#hos_H").click(function () {
                         repeat: -1,
                         ease: "none",
                       });
+                      $("#intro-img").attr("src", "../img/glitch.gif");
                     });
                 });
             });
         });
     });
 });
+
+/* FULLPAGE SETUP
+=========================== */
+(function () {
+  const second = 1000,
+    minute = second * 60,
+    hour = minute * 60,
+    day = hour * 24;
+
+  let birthday = "Sep 30, 2021 00:00:00",
+    countDown = new Date(birthday).getTime(),
+    x = setInterval(function () {
+      let now = new Date().getTime(),
+        distance = countDown - now;
+
+      (document.getElementById("days").innerText = Math.floor(distance / day)),
+        (document.getElementById("hours").innerText = Math.floor(
+          (distance % day) / hour
+        )),
+        (document.getElementById("minutes").innerText = Math.floor(
+          (distance % hour) / minute
+        )),
+        (document.getElementById("seconds").innerText = Math.floor(
+          (distance % minute) / second
+        ));
+
+      //seconds
+    }, 0);
+})();
