e7bd5c8b4790df65794e8846a339e17a62c054b8
Red Wolf ir <redwolf_ir@outlook.com>
2021-04-04 14:24:24 +0430

    add navigation and off canvas and ...

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -27,21 +27,48 @@ tl.fromTo(
     0
   )
   .fromTo(
+    ".intro ul",
+    { opacity: 0, x: 300 },
+    { duration: 3, opacity: 1, x: 0, ease: Power2.easeInOut },
+    0
+  )
+  .fromTo(
     ".curly-line",
     { opacity: 0, x: 200 },
     { duration: 3, opacity: 1, x: 0, ease: Power2.easeInOut },
     0
   );
 
+gsap.fromTo(
+  ".top-navigation",
+  { opacity: 0, y: -100 },
+  { duration: 1, opacity: 1, y: 0, ease: Power2.easeInOut },
+  0
+);
+
 /* FULLPAGE SETUP
 =========================== */
 $("#wrapper").fullpage({
   scrollHorizontally: true,
   fadingEffect: true,
-  responsiveHeight: 300,
+  responsiveHeight: 435,
+  anchors: ["intro", "featured", "works", "new-release", "about-me"],
 
   onLeave: function (index, nextIndex, direction) {
     if (index == 1 && nextIndex == 2) {
+      $(".bottom-to-top").addClass("visible");
+      gsap.fromTo(
+        ".bottom-to-top",
+        { opacity: 0, y: 200 },
+        { duration: 1, opacity: 1, y: 0, ease: Power2.easeInOut },
+        0
+      );
+      gsap.fromTo(
+        ".top-navigation",
+        { opacity: 0, y: -100 },
+        { duration: 1, opacity: 1, y: 0, ease: Power2.easeInOut },
+        0
+      );
       gsap.fromTo(
         ".curly-line-3",
         { opacity: 0, x: -50 },
@@ -73,6 +100,12 @@ $("#wrapper").fullpage({
         0
       );
     } else if (index == 2 && nextIndex == 3) {
+      gsap.fromTo(
+        ".top-navigation",
+        { opacity: 0, y: -100 },
+        { duration: 1, opacity: 1, y: 0, ease: Power2.easeInOut },
+        0
+      );
       var t2 = gsap.timeline();
 
       t2.fromTo(
@@ -86,7 +119,13 @@ $("#wrapper").fullpage({
         { duration: 2, delay: 1, opacity: 1, y: 0, ease: Power2.easeInOut },
         0
       );
-    } else if (index == 3 && direction == "down") {
+    } else if (index == 3 && nextIndex == 4) {
+      gsap.fromTo(
+        ".top-navigation",
+        { opacity: 0, y: -100 },
+        { duration: 1, opacity: 1, y: 0, ease: Power2.easeInOut },
+        0
+      );
       var t3 = gsap.timeline();
 
       t3.fromTo(
@@ -103,14 +142,54 @@ $("#wrapper").fullpage({
         )
         .fromTo(
           ".new-release .content ul",
-          { opacity: 0, y: -150 },
-          { duration: 2.5, opacity: 1, y: 0, ease: Power2.easeInOut },
+          { opacity: 0, x: 150 },
+          { duration: 2.5, opacity: 1, x: 0, ease: Power2.easeInOut },
           0
         )
         .fromTo(
           ".album-info",
           { opacity: 0, y: 150 },
           { duration: 2.5, opacity: 1, y: 0, ease: Power2.easeInOut },
+          0
+        );
+    } else if (index == 4 && direction == "down") {
+      gsap.fromTo(
+        ".top-navigation",
+        { opacity: 0, y: -100 },
+        { duration: 1, opacity: 1, y: 0, ease: Power2.easeInOut },
+        0
+      );
+
+      var t4 = gsap.timeline();
+
+      t4.fromTo(
+        ".aboutme-content",
+        { opacity: 0, x: -50 },
+        { duration: 1.5, opacity: 1, x: 0, ease: Power2.easeInOut },
+        0
+      )
+        .fromTo(
+          ".contact-me",
+          { opacity: 0, x: 50 },
+          { duration: 1.5, opacity: 1, x: 0, ease: Power2.easeInOut },
+          0
+        )
+        .fromTo(
+          ".about-me picture",
+          { opacity: 0, y: -150 },
+          { duration: 2.5, opacity: 1, y: 0, ease: Power2.easeInOut },
+          0
+        )
+        .fromTo(
+          ".about-me ul li",
+          { opacity: 0, y: 30 },
+          {
+            duration: 2.5,
+            delay: 1.5,
+            opacity: 1,
+            y: 0,
+            ease: Power2.easeInOut,
+          },
           0
         );
     }
@@ -181,7 +260,7 @@ $("#hos_H").click(function () {
                         repeat: -1,
                         ease: "none",
                       });
-                      $("#intro-img").attr("src", "../img/glitch.gif");
+                      $("#intro-img").attr("src", "assets/img/glitch.gif");
                     });
                 });
             });
@@ -217,3 +296,23 @@ $("#hos_H").click(function () {
       //seconds
     }, 0);
 })();
+
+/* OFF CANVAS
+=========================== */
+/* Open when someone clicks on the span element */
+function openNav() {
+  // document.getElementById("myNav").style.width = "100%";
+  $("#myNav").toggleClass("addWidth");
+  $(".logo").toggleClass("hide");
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  $("#myNav").toggleClass("addWidth");
+  $(".logo").toggleClass("hide");
+  if ($("#about").hasClass("opened")) {
+  }
+}
+
+/* GO TO TOP
+=========================== */
